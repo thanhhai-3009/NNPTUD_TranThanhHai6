cb44bcb1ae027523754cade6e85bac6ac41321f0
thanhhai-3009 <thanhhai3009hutech@gmail.com>
2026-03-18 14:44:53 +0700

    add changepassword and RS256

---
+++ routes/auth.js
@@ -0,0 +1,98 @@
+let express = require('express');
+let router = express.Router()
+let userController = require('../controllers/users')
+let bcrypt = require('bcrypt');
+const { CheckLogin } = require('../utils/authHandler');
+const { ChangePasswordValidator, validatedResult } = require('../utils/validator');
+let jwt = require('jsonwebtoken')
+let fs = require('fs')
+let path = require('path')
+
+const privateKey = fs.readFileSync(path.join(__dirname, '..', 'keys', 'private.pem'));
+router.post('/register', async function (req, res, next) {
+    try {
+        let { username, password, email } = req.body;
+        let newUser = await userController.CreateAnUser(username, password, email,
+            "69b1265c33c5468d1c85aad8"
+        )
+        res.send(newUser)
+    } catch (error) {
+        res.status(404).send({
+            message: error.message
+        })
+    }
+})
+
+router.post('/login', async function (req, res, next) {
+    try {
+        let { username, password } = req.body;
+        let user = await userController.GetAnUserByUsername(username);
+        if (!user) {
+            res.status(404).send({
+                message: "thong tin dang nhap khong dung"
+            })
+            return;
+        }
+        if (user.lockTime > Date.now()) {
+            res.status(404).send({
+                message: "ban dang bi ban"
+            })
+            return;
+        }
+        if (bcrypt.compareSync(password, user.password)) {
+            user.loginCount = 0;
+            await user.save()
+            let token = jwt.sign({
+                id: user._id
+            }, privateKey, {
+                algorithm: 'RS256',
+                expiresIn: '1d'
+            })
+            res.send(token)
+        } else {
+            user.loginCount++;
+            if (user.loginCount == 3) {
+                user.loginCount = 0;
+                user.lockTime = Date.now() + 3600 * 1000;
+            }
+            await user.save()
+            res.status(404).send({
+                message: "thong tin dang nhap khong dung"
+            })
+        }
+    } catch (error) {
+        res.status(404).send({
+            message: error.message
+        })
+    }
+})
+router.get('/me', CheckLogin, function (req, res, next) {
+    res.send(req.user)
+})
+
+router.post('/changepassword', CheckLogin, ChangePasswordValidator, validatedResult, async function (req, res, next) {
+    try {
+        let { oldpassword, newpassword } = req.body;
+        let isMatched = bcrypt.compareSync(oldpassword, req.user.password);
+
+        if (!isMatched) {
+            res.status(400).send({
+                message: "oldpassword khong dung"
+            })
+            return;
+        }
+
+        req.user.password = newpassword;
+        await req.user.save();
+
+        res.send({
+            message: "doi mat khau thanh cong"
+        })
+    } catch (error) {
+        res.status(400).send({
+            message: error.message
+        })
+    }
+})
+
+module.exports = router
